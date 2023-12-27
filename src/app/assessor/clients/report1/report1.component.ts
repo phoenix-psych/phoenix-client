485357f4e9bb1fafd6d00269dc9c7ae5a518cc9c
SJ <sjiffin20@gmail.com>
2023-12-27 22:36:39 +0530

    report 2 : 1

--- src/app/assessor/clients/report1/report1.component.ts
+++ src/app/assessor/clients/report1/report1.component.ts
@@ -34,101 +34,44 @@ export class Report1Component implements OnInit {
 
   public generatePDF(): void {
     
-    this.generatePDF3();
-    // html2canvas(this.invoiceElement.nativeElement, { scale: 3 }).then((canvas) => {
-    //   const imageGeneratedFromTemplate = canvas.toDataURL('image/png');
-    //   const fileWidth = 200;
-    //   const generatedImageHeight = (canvas.height * fileWidth) / canvas.width;
-    //   let PDF = new jsPDF('p', 'mm', 'a4',);
-    //   PDF.addImage(imageGeneratedFromTemplate, 'PNG', 0, 5, fileWidth, generatedImageHeight,);
-    //   PDF.html(this.invoiceElement.nativeElement.innerHTML)
-    //   PDF.save(`${this.data.name}-report.pdf`);
-    // });
+    this.generatePDF5();
   }
 
   getAge() {
-        const date: Date = this.data.dob;
-        // let timeDiff = Math.abs(Date.now() - date.getTime());
-        // this.age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+    this.age = this.calculateAge(this.data.dob);
   }
 
-  //tiles: string[] = ['Tile 1', 'Tile 2', 'Tile 3'];
+  calculateAge(birthDate: Date): number {
+    const today = new Date();
+    const birth = new Date(birthDate);
+    let age = today.getFullYear() - birth.getFullYear();
+    const monthDiff = today.getMonth() - birth.getMonth();
 
-  // generatePDF1() {
-  //   const pdfDoc = new jsPDF({
-  //     orientation: 'l',
-  //     unit: 'pt',
-  //     format: [612, 792],
-  //   });
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
+      age--;
+    }
 
-  //   const margins = {
-  //     top: 36,
-  //     bottom: 36,
-  //     left: 36,
-  //     width: 720,
-  //     height: 540,
-  //   };
-
-  //   const pageLength = this.invoiceElement.nativeElement.children.length;
-  //   const pageCanvases = new Array(pageLength);
-  //   const canvasOperations: Promise<HTMLCanvasElement>[] = [];
-
-  //   for (let i = 0; i < pageLength; i++) {
-  //     const pdfHTML = this.invoiceElement.nativeElement.children[i];
-  //     canvasOperations.push(html2canvas(pdfHTML));
-  //   }
-
-  //   Promise.all(canvasOperations).then((canvases) => {
-  //     canvases.forEach((canvas, idx) => {
-  //       if (idx > 0) {
-  //         pdfDoc.addPage();
-  //       }
-
-  //       const imgData = canvas.toDataURL('image/jpeg');
-  //       pdfDoc.addImage(imgData, 'JPEG', margins.left, margins.top, margins.width, margins.height);
-  //     });
-
-  //     pdfDoc.save('file2.pdf');
-  //   });
-  // }
-
-  // generatePDF2(): void {
-    
-  //   const pdfDoc = new jsPDF({
-  //     orientation: 'l',
-  //     unit: 'pt',
-  //     format: [612, 792] // 72pts per inch
-  //   });
-
-  //   const margins = {
-  //     top: 36,
-  //     bottom: 36,
-  //     left: 36,
-  //     width: 720,
-  //     height: 540
-  //   };
-
-  //   const pdfHTML = this.invoiceElement.nativeElement;
-
-  //   html2canvas(pdfHTML).then((canvas) => {
-  //     const img = canvas.toDataURL();
-  //     pdfDoc.addImage(img, 'jpg', margins.left, margins.top, margins.width, margins.height);
-  //     pdfDoc.save(`Report:${this.data.name}(${this.data.email}).pdf`);
-  //   });
-  // }
+    return age;
+  }
 
   generatePDF3(){
     const data = this.invoiceElement.nativeElement;
     html2canvas(data).then((canvas:any) => {
-      const imgWidth = 208;
-      const pageHeight = 295;
-      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+      const doc = new jsPDF('p', 'mm', 'a4');
+
+      // const imgWidth = 208;
+      // const pageHeight = 295;
+
+      const imgWidth = doc.internal.pageSize.getWidth();
+      const pageHeight = doc.internal.pageSize.getHeight();
+
+      const imgHeight = Math.ceil((canvas.height * imgWidth) / canvas.width);
       console.log(`imgHeight - ${imgHeight}`);
       let heightLeft = imgHeight;
       let position = 0;
       let pageNo = 0;
       heightLeft -= pageHeight;
-      const doc = new jsPDF('p', 'mm', 'a4');
       doc.addImage(canvas, 'PNG', 0, position, imgWidth, imgHeight, '', 'FAST');
       while (heightLeft >= 0) {
         position = heightLeft - imgHeight;
@@ -138,6 +81,39 @@ export class Report1Component implements OnInit {
       }
       doc.save(`Report:${this.data.name}(${this.data.email}).pdf`);
     });
+  }
+
+  generatePDF5() {
+    const pages = document.querySelectorAll('.page');
+    const pdf = new jsPDF('p', 'mm', 'a4');
+  
+    function generatePage(index:number) {
+      if (index < pages.length) {
+        const page = pages[index] as HTMLElement;
+  
+        // Use html2canvas to capture the HTML content as an image
+        html2canvas(page).then(canvas => {
+          const imageData = canvas.toDataURL('image/png');
+  
+          // Add a new page to the PDF
+          if (index > 0) {
+            pdf.addPage();
+          }
+  
+          // Add the captured image to the PDF
+          pdf.addImage(imageData, 'PNG', 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight());
+  
+          // Move to the next page
+          generatePage(index + 1);
+        });
+      } else {
+        // Save or display the PDF
+        pdf.save('page-wise-pdf.pdf');
+      }
+    }
+  
+    // Start generating PDF from the first page
+    generatePage(0);
   }
 
 }
