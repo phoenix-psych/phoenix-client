a85075b9cb3519e13423fa81f73377562c8c0d71
SJ <sjiffin20@gmail.com>
2024-02-19 08:02:38 +0530

    19-2

--- src/app/assessor/clients/report1/report1.component.ts
+++ src/app/assessor/clients/report1/report1.component.ts
@@ -1334,11 +1334,6 @@ export class Report1Component implements OnInit {
    };
   }
 
-  public generatePDF(): void {
-    
-    this.generatePDF5();
-  }
-
   OnEdit() {
     this.isEdit = !this.isEdit
     if(this.isEdit)
@@ -1370,66 +1365,33 @@ export class Report1Component implements OnInit {
     return age;
   }
 
-  generatePDF3(){
-    const data = this.invoiceElement.nativeElement;
-    html2canvas(data).then((canvas:any) => {
-
-      const doc = new jsPDF('p', 'mm', 'a4');
-
-      // const imgWidth = 208;
-      // const pageHeight = 295;
-
-      const imgWidth = doc.internal.pageSize.getWidth();
-      const pageHeight = doc.internal.pageSize.getHeight();
-
-      const imgHeight = Math.ceil((canvas.height * imgWidth) / canvas.width);
-      console.log(`imgHeight - ${imgHeight}`);
-      let heightLeft = imgHeight;
-      let position = 0;
-      let pageNo = 0;
-      heightLeft -= pageHeight;
-      doc.addImage(canvas, 'PNG', 0, position, imgWidth, imgHeight, '', 'FAST');
-      while (heightLeft >= 0) {
-        position = heightLeft - imgHeight;
-        doc.addPage();
-        doc.addImage(canvas, 'PNG', 0, position, imgWidth, imgHeight, '', 'FAST');
-        heightLeft -= pageHeight;
-      }
-      doc.save(`Report:${this.data.name}(${this.data.email}).pdf`);
-    });
-  }
-
-  generatePDF5() {
+  public generatePDF(): void {
     const pages = document.querySelectorAll('.page');
     const pdf = new jsPDF('p', 'mm', 'a4');
+    const name = this.data.name;
   
-    function generatePage(index:number) {
-      if (index < pages.length) {
+    function generatePage(name:string ,index:number) {
+      // if (index < pages.length) {
+        if (index < 2) {
         const page = pages[index] as HTMLElement;
   
-        // Use html2canvas to capture the HTML content as an image
         html2canvas(page).then(canvas => {
           const imageData = canvas.toDataURL('image/png');
   
-          // Add a new page to the PDF
           if (index > 0) {
             pdf.addPage();
           }
   
-          // Add the captured image to the PDF
           pdf.addImage(imageData, 'PNG', 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight());
   
-          // Move to the next page
-          generatePage(index + 1);
+          generatePage(name, index + 1);
         });
       } else {
-        // Save or display the PDF
-        pdf.save('page-wise-pdf.pdf');
+        pdf.save(`${name}.pdf`);
       }
     }
   
-    // Start generating PDF from the first page
-    generatePage(0);
+    generatePage(name, 0);
   }
 
   selectedSPLDDesc = '';
